fe4d85d8bfc3907cf4ae45a5cc04e159d8a5bbf7
Pratham Gandhi <ppg.1382@gmail.com>
2022-02-26 14:54:59 +0530

    Created the views for registration

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserSerializer
 
 # Create your views here.
+
+class Register(APIView):
+
+    def post(self, request):
+        userSerializer = UserSerializer(data=request.data)
+        if userSerializer.is_valid():
+            userSerializer.save()
+            return Response({'Status' : 'Success'}, status=status.HTTP_201_CREATED)
+        elif userSerializer.errors['username'][0].code == 'unique':
+            return Response({'Status' : 'Failure... User already registered'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        else:
+            return Response({'Statuts' : 'Failure... Some error occurred'}, status=status.HTTP_400_BAD_REQUEST)
