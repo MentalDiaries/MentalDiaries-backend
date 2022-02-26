043c625262f04e0bb18fe0d24d57e4a4575dfd73
Pratham Gandhi <ppg.1382@gmail.com>
2022-02-26 16:02:46 +0530

    Created a serializer for GETting the entry

---
+++ apps/diary/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Entry
+
+class EntryGetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Entry
+        exclude = ['user']
