b5a8b41caba3f715e2327d07f05fe5a6bc9a0ff8
Pratham Gandhi <ppg.1382@gmail.com>
2022-02-26 14:57:10 +0530

    Added the register view to urls

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
+from .views import Register
 
 urlpatterns = [
-    path('login/', jwt_views.TokenObtainPairView.as_view(),
+     path('register/', Register.as_view(), name='register'),
+     path('login/', jwt_views.TokenObtainPairView.as_view(),
          name='obtain_token_pair'),
-    path('login/refresh/', jwt_views.TokenRefreshView.as_view(),
+     path('login/refresh/', jwt_views.TokenRefreshView.as_view(),
          name='refresh_token'),
 ]
